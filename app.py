8ab71237ef9edc6d0a9fccff6ebf1c864ef38eac
caovy2001 <caovy2001@gmail.com>
2022-11-21 13:12:05 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,165 @@
+import json
+from flask import Flask, jsonify, request
+import nltk
+# nltk.download('punkt')
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+from tensorflow.python.framework import ops
+import random
+import json
+import pickle
+import os;
+
+
+app = Flask(__name__)
+
+@app.route("/predict", methods=['POST'])
+def predictAPI():
+    payload = json.loads(request.data)
+    text = payload["text"]
+    username = payload["username"]
+    intent_result = predict(text, username)
+
+    return jsonify({
+        "intent": intent_result[0],
+        "accuracy": str(intent_result[1])
+    })
+
+@app.route("/train", methods=['POST'])
+def trainAPI():
+    payload = json.loads(request.data)
+    username = payload["username"]
+    trainingHistoryId = payload["training_history_id"]
+
+    if not os.path.exists(username):
+        os.makedirs(username)
+
+    with open(username + "/intents.json", "w+") as outfile:
+        outfile.write(json.dumps(payload, indent=4))
+
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in payload["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern["content"])
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["name"])
+
+        if intent["name"] not in labels:
+            labels.append(intent["name"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open(username + "/data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+    ops.reset_default_graph()
+
+    net = tflearn.input_data(shape=[None, len(training[0])])
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+    net = tflearn.regression(net)
+
+    model = tflearn.DNN(net)
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save(username + "/model.tflearn")
+
+    return jsonify({
+        "trainingHistoryId": trainingHistoryId
+    })
+
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+            
+    return numpy.array(bag)
+
+def predict(text, username):
+    # Load model 
+    with open(username + "/data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+    ops.reset_default_graph()
+    net = tflearn.input_data(shape=[None, len(training[0])])
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+    net = tflearn.regression(net)
+    
+    model = tflearn.DNN(net)
+    model.load(username + "/model.tflearn")
+
+    # Load words và labels 
+    words = []
+    labels = []
+
+    with open(username + "/intents.json") as file:
+        data = json.load(file)
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern["content"])
+            words.extend(wrds)
+
+        if intent["name"] not in labels:
+            labels.append(intent["name"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+    labels = sorted(labels)
+
+    # predict
+    results = model.predict([bag_of_words(text, words)])
+    results_index = numpy.argmax(results)
+    tag = labels[results_index]
+    return [tag, results[0][results_index]]
+
+
+@app.route("/")
+def index():
+    return "Welcome!"
